cf5c19b902654a5b1af3c5abe5cfbf4ab336bef4
pius712 <aksfbsgnlfls@gmail.com>
2023-09-26 18:13:45 +0900

    fix: timestamp 안 먹는거 수정

--- src/main/kotlin/org/cloudnovel/novel/core/storage/BaseEntity.kt
+++ src/main/kotlin/org/cloudnovel/novel/core/storage/BaseEntity.kt
@@ -4,6 +4,8 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
@@ -12,13 +14,15 @@ import java.time.LocalDateTime
 abstract class BaseEntity() {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 
 
-    @CreatedDate
+//    @CreatedDate
+    // 위에는 안되고, 아래는 됨;
+    @CreationTimestamp
     private val createDate: LocalDateTime? = null;
 
-    @LastModifiedDate
+    @UpdateTimestamp
     private val lastModifiedDate: LocalDateTime? = null;
 }
